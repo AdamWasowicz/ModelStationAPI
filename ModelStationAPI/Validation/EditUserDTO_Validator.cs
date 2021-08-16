d0d03819bd7a4ee131496b62f2df05f02e0f2904
Adam Wąsowicz <adam.wasowicz@student.po.edu.pl>
2021-08-16 15:24:59 +0200

    update CreateUserDTO_Validator

---
+++ ModelStationAPI/Validation/EditUserDTO_Validator.cs
@@ -0,0 +1,39 @@
+﻿using FluentValidation;
+using ModelStationAPI.Entities;
+using ModelStationAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModelStationAPI.Validation
+{
+    public class EditUserDTO_Validator : AbstractValidator<EditUserDTO>
+    {
+        private readonly List<char> Genders = new List<char> { 'F', 'M' };
+
+        public EditUserDTO_Validator()
+        {
+            RuleFor(u => u.Name)
+                .MaximumLength(64);
+
+
+            RuleFor(u => u.Surname)
+                .MaximumLength(64);
+
+
+            RuleFor(u => u.Gender)
+                .Custom((value, context) =>
+                {
+                    var validGender = Genders.Contains(value);
+
+                    if (!validGender)
+                        context.AddFailure("Gendr", "NOT VALID");
+                });
+
+
+            RuleFor(u => u.Description)
+                .MaximumLength(256);
+        }
+    }
+}

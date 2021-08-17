f4f031cf26044562a57b97c9f62651120d420e52
Adam Wąsowicz <adam.wasowicz@student.po.edu.pl>
2021-08-17 12:18:31 +0200

    add CreatePostCategoryDTO_Validator

---
+++ ModelStationAPI/Validation/CreatePostCategoryDTO_Validator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using ModelStationAPI.Entities;
+using ModelStationAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModelStationAPI.Validation
+{
+    public class CreatePostCategoryDTO_Validator : AbstractValidator<CreatePostCategoryDTO>
+    {
+        public CreatePostCategoryDTO_Validator(ModelStationDbContext dbContext)
+        {
+            RuleFor(pc => pc.Name)
+                .NotEmpty()
+                .MaximumLength(32)
+                .Custom((value, context) =>
+                {
+                    var nameInUse = dbContext.PostCategories.Any(pc => pc.Name == value);
+                    if (nameInUse)
+                        context.AddFailure("Name", "TAKEN");
+                });
+
+            RuleFor(pc => pc.Description)
+                .MaximumLength(64);
+        }
+    }
+}
